0d79a7a43a8f9334b98adff02bcf45ca42c38e64
Gwon <121155771+shingigwon@users.noreply.github.com>
2025-01-16 18:32:10 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 141 ms, Memory: 19,720 KB -BaekjoonHub

---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/README.md
@@ -0,0 +1,15 @@
+# [D4] [S/W 문제해결 기본] 7일차 - 미로1 - 1226 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14vXUqAGMCFAYD) 
+
+### 성능 요약
+
+메모리: 19,720 KB, 시간: 141 ms, 코드길이: 1,450 Bytes
+
+### 제출 일자
+
+2025-01-16 18:32
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int[][] arr;
+	static boolean[][] visited;
+	static int[] dx = {-1,1,0,0}; //상 하 
+	static int[] dy = {0,0,-1,1}; //좌 우
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		for (int t = 0; t < 10; t++) {
+			int T = Integer.parseInt(br.readLine());
+
+			arr = new int[16][16];
+			visited = new boolean[16][16];
+			
+			int startx = -1;
+			int starty = -1;
+			
+			for (int i = 0; i < 16; i++) {
+				String[] str = br.readLine().split("");
+				for (int j = 0; j < 16; j++) {
+					int num = Integer.parseInt(str[j]);
+					arr[i][j] = num;
+					if(num==2) {
+						startx = i;
+						starty = j;
+					}
+						
+					
+				}
+			}
+			boolean check = false;
+			check = DFS(startx,starty);
+			System.out.printf("#%d %d\n", T, check?1:0);
+			
+		}
+		br.close();
+	}
+	
+	static boolean DFS(int x, int y) {
+		Stack<int[]> stack = new Stack<int[]>();
+		stack.push(new int[] {x,y});
+		visited[x][y] = true;
+		
+		while(!stack.isEmpty()) {
+			int[] cur = stack.pop();
+			int cx = cur[0];
+			int cy = cur[1];
+						
+			for(int i=0; i<4; i++) {
+				int nx = cx+dx[i];
+				int ny = cy+dy[i];
+				
+				if(arr[nx][ny]==3)
+					return true;
+				
+				if(nx>=0&&nx<16&&ny>=0&&ny<16&&arr[nx][ny]==0&&!visited[nx][ny]) {
+					visited[nx][ny]=true;
+					stack.push(new int[] {nx,ny});
+				}
+			}
+			
+		}
+		
+		return false;
+	}
+}
