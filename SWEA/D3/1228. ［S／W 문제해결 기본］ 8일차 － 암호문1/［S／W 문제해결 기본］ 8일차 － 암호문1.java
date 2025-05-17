fa119b9dcfb32d2acd1959fb45eb7c8ca00c5292
Gwon <121155771+shingigwon@users.noreply.github.com>
2025-05-17 17:58:24 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 82 ms, Memory: 25,984 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		for (int t = 1; t <= 10; t++) {
+			List<String> list = new ArrayList<String>();
+			
+			int N = Integer.parseInt(br.readLine());
+			
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<N; i++) 
+				list.add(st.nextToken());
+			
+			int K = Integer.parseInt(br.readLine());
+			
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<K; i++) {
+				String cmd = st.nextToken();
+				int X = Integer.parseInt(st.nextToken());
+				int Y = Integer.parseInt(st.nextToken());
+				
+				for(int j=X; j<X+Y; j++) 
+					list.add(j,st.nextToken());
+			}
+			StringBuilder sb = new StringBuilder();
+			
+			for(int i=0; i<10; i++)
+				sb.append(list.get(i)).append(" ");
+			
+			System.out.printf("#%d %s\n", t,sb.toString());
+		}
+		br.close();
+	}
+}
+
