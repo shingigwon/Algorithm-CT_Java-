326e5626a96b1e676b4940d386de84e41512e2ac
Gwon <121155771+shingigwon@users.noreply.github.com>
2026-04-01 21:56:03 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,58 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[] dy = {1,-1,0,0};
-	static int[] dx = {0,0,-1,1};
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[][] map = new int[N][M];
-		boolean[][] vis = new boolean[N][M];
-		
-		for(int i=0; i<N; i++) {
-			String[] input = br.readLine().split("");
-			Arrays.fill(vis[i], false);
-			for(int j=0; j<M; j++) {
-				int n = Integer.parseInt(input[j]);
-				map[i][j] = n;
-				if(n==0)
-					vis[i][j] = true;
-			}
-		}
-		System.out.println(Func(map, vis, N, M));
-		br.close();
-	}
-	
-	static int Func(int[][] map, boolean[][] vis, int N, int M) {
-		PriorityQueue<int[]> pq = new PriorityQueue<int[]>((o1, o2)-> Integer.compare(o1[2], o2[2]));
-		pq.add(new int[]{0,0,1});
-		
-		while(!pq.isEmpty()) {
-			int[] cur = pq.poll();
-			int cy = cur[0];
-			int cx = cur[1];
-			int cost = cur[2];
-			
-			if(cy == N-1 && cx == M-1)
-				return cost;
-			
-			
-			for(int i=0; i<4; i++) {
-				int ny = cy+dy[i];
-				int nx = cx+dx[i];
-				
-				if(ny<N && ny>=0 && nx>=0 && nx<M && !vis[ny][nx]) {
-					pq.add(new int[] {ny,nx, cost+1});
-					vis[ny][nx] = true;
-				}
-			}
-		}
-		
-		return -1;
-	}
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {1,-1,0,0};
 
+    static int N;
+    static int M;
+
+    static int[][] map;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for(int i=0; i<N; i++){
+            String input = br.readLine();
+            for(int j=0; j<M; j++){
+                map[i][j] = input.charAt(j) - '0';
+            }
+        }
+
+        bfs(0,0);
+
+        System.out.println(map[N-1][M-1]);
+        br.close();
+    }
+    static void bfs(int x, int y){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[] {x,y});
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            if(cx == N-1 && cy == M-1)
+                return;
+
+            for(int i=0; i<4; i++){
+                int nx = cx+dx[i];
+                int ny = cy+dy[i];
+
+                if(nx<0 || nx>=N || ny<0 || ny>=M)
+                    continue;
+                if(map[nx][ny]!=1)
+                    continue;
+
+                map[nx][ny] = map[cx][cy]+1;
+                q.add(new int[]{nx, ny});
+
+            }
+        }
+    }
 }
-
