43bc72e8b3af334aa36232b31dd4fc62eb7dddca
Gwon <121155771+shingigwon@users.noreply.github.com>
2026-04-01 22:15:52 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 15968 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,56 +2,75 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[][] graph;
-	static int M;
-	static int N;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int T = Integer.parseInt(br.readLine());
-		
-		
-		for(int i=0; i<T; i++) {
-			st = new StringTokenizer(br.readLine());
-			M = Integer.parseInt(st.nextToken());//10
-			N = Integer.parseInt(st.nextToken());//8
-			int K = Integer.parseInt(st.nextToken());
-			
-			graph = new int[N][M];
-			
-			for(int j=0; j<K; j++) {
-				st = new StringTokenizer(br.readLine());
-				int Y = Integer.parseInt(st.nextToken());//10
-				int X = Integer.parseInt(st.nextToken());//8
-				graph[X][Y]=1;
-			}
-			
-			int result = 0;
-	        for(int j = 0; j < N; j++) {
-	            for (int k = 0; k < M; k++) {
-	                if (dfs(j,k)) {
-	                    result += 1;
-	                }
-	            }
-	        }
-			
-	        System.out.println(result);
-		}
-		br.close();
-	}
-	
-	public static boolean dfs(int x, int y) {
-       if (x <= -1 || x >= N || y <= -1 || y >= M) {
-           return false;
-       }
-       if (graph[x][y] == 1) {
-           graph[x][y] = 2;
-           dfs(x-1, y); //상
-           dfs(x, y-1); // 좌
-           dfs(x+1, y); // 하
-           dfs(x, y+1); // 우
-           return true;
-       }
-       return false;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+
+    static int M;
+    static int N;
+    static int K;
+
+    static int[][] map;
+    static int res;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int TC = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < TC; t++) {
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+
+            map = new int[M][N];
+            res = 0;
+
+            for(int i=0; i<K; i++){
+                st = new StringTokenizer(br.readLine());
+                int X = Integer.parseInt(st.nextToken());
+                int Y = Integer.parseInt(st.nextToken());
+                map[X][Y] = 1;
+            }
+
+            for(int i=0; i<M; i++){
+                for(int j=0; j<N; j++){
+                    if(map[i][j]==0)
+                        continue;
+                    bfs(i,j);
+                }
+            }
+            
+            System.out.println(res);
+        }
+        br.close();
+    }
+
+    static void bfs(int x, int y) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{x, y});
+        map[x][y] = 0;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx < 0 || nx >= M || ny < 0 || ny >= N)
+                    continue;
+
+                if (map[nx][ny] != 1)
+                    continue;
+
+                map[nx][ny] = 0;
+                q.add(new int[]{nx, ny});
+            }
+        }
+        res++;
     }
 }
