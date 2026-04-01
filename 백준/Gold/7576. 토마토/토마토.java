40e67c92c8a77b3a533ff93ddadd5f3c033b1118
Gwon <121155771+shingigwon@users.noreply.github.com>
2026-04-01 22:55:38 +0900

    [Gold V] Title: 토마토, Time: 612 ms, Memory: 102776 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -2,57 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
 
-		int[] dx = {0,0,-1,1};
-		int[] dy = {1,-1,0,0};
-		
-		int[][] map = new int[N][M];
-		Queue<int[]> q = new LinkedList<int[]>();
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			
-			for(int j=0; j<M; j++) {
-				int t = Integer.parseInt(st.nextToken());
-				map[i][j] = t;
-				
-				if(t==1)
-					q.add(new int[] {i,j});
-				
-				
-			}
-		}
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			int y = cur[0];
-			int x = cur[1];
-			
-			for(int i=0; i<4; i++) {
-				int ny = y+dy[i];
-				int nx = x+dx[i];
-				
-				if(ny>=0 && ny<N && nx>=0 && nx<M && map[ny][nx]==0) {
-					q.add(new int[] {ny,nx});
-					map[ny][nx] = map[y][x]+1;
-				}
-			}
-		}
-		boolean check = Arrays.stream(map)
-			    .flatMapToInt(Arrays::stream)
-			    .anyMatch(v->v==0);
-		
-		int max = check?-1:Arrays.stream(map)
-				.flatMapToInt(Arrays::stream)
-				.max()
-				.getAsInt()-1;
-		
-		System.out.println(max);
-		br.close();
-	}
-	
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[N][M];
+        Queue<int[]> q = new ArrayDeque<>();
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<M; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 1)
+                    q.add(new int[] {i,j});
+            }
+        }
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for(int i=0; i<4; i++){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+
+                if(nx<0 || nx>=N || ny<0 || ny>=M)
+                    continue;
+
+                if(map[nx][ny]!=0)
+                    continue;
+
+                map[nx][ny] = map[x][y]+1;
+                q.add(new int[] {nx,ny});
+            }
+        }
+
+        int res = 0;
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                if(map[i][j]==0){
+                    res = -1;
+                    break;
+                }
+                res = Math.max(res, map[i][j]);
+            }
+            if(res == -1)
+                break;
+        }
+
+        System.out.println(res == -1 ? -1 : res-1);
+        br.close();
+    }
 }
