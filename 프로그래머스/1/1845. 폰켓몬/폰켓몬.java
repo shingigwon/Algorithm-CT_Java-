6be274671a2a71909b949a142ea263be1b452e8d
Gwon <121155771+shingigwon@users.noreply.github.com>
2026-04-17 00:39:51 +0900

    [level 1] Title: 폰켓몬, Time: 3.66 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,12 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet<Integer>();
-    	int max = nums.length/2;
-    	
-    	for(int i:nums)
-    		set.add(i);
-    	
-    	return Math.min(max, set.size());
+        Set<Integer> set = new HashSet<>();
+        for(int i : nums){
+            set.add(i);
+        }
+        return Math.min(nums.length/2, set.size());
     }
 }
