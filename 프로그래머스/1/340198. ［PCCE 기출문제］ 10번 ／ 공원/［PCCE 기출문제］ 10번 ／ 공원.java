4f6ad008513ff5d3c26cf1fdfaa036af259ae82f
Gwon <121155771+shingigwon@users.noreply.github.com>
2026-04-18 19:22:29 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 1.28 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,47 +1,34 @@
 import java.util.Arrays;
 
 class Solution {
-
-    boolean[][] map;
     public int solution(int[] mats, String[][] park) {
         int R = park.length;
         int C = park[0].length;
 
-        map = new boolean[R][C];
+        int[][] dp = new int[R][C];
+
+        int max = 0;
 
         for(int i=0; i<R; i++){
             for(int j=0; j<C; j++){
                 if(park[i][j].equals("-1")){
-                    map[i][j] = true;
+                    if(i==0 || j==0)
+                        dp[i][j] = 1;
+
+                    else
+                        dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1])+1;
+
+                    max = Math.max(max, dp[i][j]);
                 }
             }
         }
 
         Arrays.sort(mats);
-
-        for(int idx = mats.length-1; idx>=0; idx--){
-            int mat = mats[idx];
-
-            for (int i = 0; i < R; i++) {
-                for (int j = 0; j < C; j++) {
-                    if (!map[i][j]) continue;
-
-                    if(i+mat > R || j+mat > C) continue;
-
-                    if(isRectangle(i,j,mat)) return mat;
-                }
-            }
+        for(int i=mats.length-1; i>=0; i--){
+            if(mats[i]<=max)
+                return mats[i];
         }
 
         return -1;
-    }
-
-    boolean isRectangle(int r, int c, int size){
-        for(int i=r; i<r+size; i++){
-            for(int j=c; j<c+size; j++){
-                if(!map[i][j]) return false;
-            }
-        }
-        return true;
     }
 }
