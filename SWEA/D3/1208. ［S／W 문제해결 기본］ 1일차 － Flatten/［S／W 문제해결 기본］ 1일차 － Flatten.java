eebc8bda1460a97baccf504929667b8a1acad52c
Gwon <121155771+shingigwon@users.noreply.github.com>
2026-05-15 20:39:06 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 160 ms, Memory: 33,024 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,35 +1,35 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int T = 10;
-		
-		for(int t=1; t<T+1; t++) {
-			List<Integer> list = new ArrayList<>(100);
-			int count = Integer.parseInt(br.readLine());
-			
-			st = new StringTokenizer(br.readLine());
-			for(int i=0; i<100; i++) {
-				list.add(Integer.parseInt(st.nextToken()));
-			}
-			
-			for(int i=0; i<count; i++) {
-				int maxidx = list.indexOf(Collections.max(list));
-				int minidx = list.indexOf(Collections.min(list));
-				list.set(maxidx, list.get(maxidx)-1);
-				list.set(minidx, list.get(minidx)+1);
-			}
-			
-			int result = Collections.max(list)-Collections.min(list);
-            System.out.printf("#%d %d\n", t, result);
-		}
-		
-		
-		
-		br.close();
-	}
+class Solution{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        for(int tc = 1; tc<=10; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+
+            int[] arr = new int[100];
+            for(int i=0; i<100; i++){
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            for(int i=0; i<N; i++){
+                int minIdx = 0;
+                int maxIdx = 0;
+
+                for(int j=0; j<100; j++){
+                    if(arr[j]>arr[maxIdx]) maxIdx = j;
+                    if(arr[j]<arr[minIdx]) minIdx = j;
+                }
+                arr[maxIdx]--;
+                arr[minIdx]++;
+            }
+
+            int max = Arrays.stream(arr).max().getAsInt();
+            int min = Arrays.stream(arr).min().getAsInt();
+
+            System.out.printf("#%d %d\n",tc, max-min);
+        }
+    }
 }
