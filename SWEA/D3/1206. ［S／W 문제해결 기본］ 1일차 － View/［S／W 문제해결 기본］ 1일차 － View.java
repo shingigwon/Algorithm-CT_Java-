61088854520c04124905c1e7b8f8890c0087d9eb
Gwon <121155771+shingigwon@users.noreply.github.com>
2026-05-18 00:41:00 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 95 ms, Memory: 27,648 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,41 +1,38 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+class Solution{
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-
-		int T = 10;
-
-		for (int i = 1; i < T+1; i++) {
+		for(int tc = 1; tc<=10; tc++) {
 			int N = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
 			
 			int[] arr = new int[N];
 			
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				arr[j] = Integer.parseInt(st.nextToken());
+			for(int i=0; i<N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
 			}
 			
-			int sum = 0;
-			for(int j=2; j<N-2; j++) {
-				int max = Integer.MIN_VALUE;
+			int res = 0;
+			for(int i=2; i<arr.length-2; i++) {
+				int max = Math.max(
+						Math.max(arr[i-2], arr[i-1]),
+						Math.max(arr[i+2], arr[i+1])
+						);
 				
-				for(int k=j-2; k<=j+2; k++) {
-					if(k==j)
-						continue;
-					else if(max<arr[k])
-						max = arr[k];
-				}
-				if(arr[j]>max)
-					sum+=arr[j] - max;
+				if(max>=arr[i]) continue;
+				
+				res += arr[i]-max;
 			}
-			System.out.printf("#%d %d\n",i,sum);
-
+			
+			
+			sb.append("#").append(tc).append(" ").append(res).append("\n");
 		}
-
-		br.close();
-	}
-
+		
+		System.out.println(sb.toString());
+	}	
 }
